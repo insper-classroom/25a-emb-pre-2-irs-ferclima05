ad3d902b88b623aaf0ce665d0614970159e1cfdf
ferclima05 <fernandocl3@al.insper.edu.br>
2025-02-14 17:48:36 -0300

    Exercício 1

--- exe1/main.c
+++ exe1/main.c
@@ -2,17 +2,20 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN_R = 28;
+volatile int flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    printf("fall \n");
-  } else if (events == 0x8) { // rise edge
-    printf("rise \n");
-  }
+  //if (events == 0x4) { // fall edge
+  //  printf("fall \n");
+  //} else if (events == 0x8) { // rise edge
+  //  printf("rise \n");
+  //}
+  flag = 1;
 }
 
 int main() {
+  const int BTN_PIN_R = 28;
+
   stdio_init_all();
 
   gpio_init(BTN_PIN_R);
@@ -23,5 +26,12 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+    if(!gpio_get(BTN_PIN_R)){
+      printf("fall\n");
+      while (!gpio_get(BTN_PIN_R)){
+      };
+      printf("rise\n");
+    }
+    flag = 0;
   }
 }
